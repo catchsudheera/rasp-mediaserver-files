0da4bf9ff5ae7301a00fb56ebba1572762bf9a32
Sudheera Palihakkara <catchsudheera@gmail.com>
2020-03-08 10:40:44 +0100

    Added waiting for expressvpn deamon to start

--- bash-scripts/expressvpn-connect.sh
+++ bash-scripts/expressvpn-connect.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-/usr/bin/expressvpn connect 1>/var/log/expressvpn-start/connet.log
+rm /var/log/expressvpn-start/connet.log 
+echo "starting" > /var/log/expressvpn-start/connet.log
+until service expressvpn status 1>/dev/null
+do
+  sleep 10
+done
+
+date>>/var/log/expressvpn-start/connet.log
+until /usr/bin/expressvpn connect 1>>/var/log/expressvpn-start/connet.log
+do
+  sleep 10
+done
